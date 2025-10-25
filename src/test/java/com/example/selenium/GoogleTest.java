eddd0c66f8dc9ef198909918106374b92f1974e5
Shranya <shranya.212084202@vcet.edu.in>
2025-10-25 14:23:15 +0530

    Selenium

---
+++ src/test/java/com/example/selenium/GoogleTest.java
@@ -0,0 +1,25 @@
+package com.example.selenium;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleTest {
+
+    @Test
+    void openGoogleHomePage() {
+        // Automatically set up ChromeDriver
+        WebDriverManager.chromedriver().setup();
+
+        // Launch Chrome
+        WebDriver driver = new ChromeDriver();
+
+        // Open Google
+        driver.get("https://www.google.com");
+        System.out.println("Page Title: " + driver.getTitle());
+
+        // Close browser
+        driver.quit();
+    }
+}
